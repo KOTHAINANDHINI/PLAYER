c0adb61d21776c50fe45ef1be11adaa5c6638877
KOTHAINANDHINI <46838428+KOTHAINANDHINI@users.noreply.github.com>
2019-02-16 03:26:29 +0530

    Update 11.C

--- 11.C
+++ 11.C
@@ -1,20 +1,27 @@
 #include<stdio.h>
 
 int main() {
-  char a[100];
-  int i,l=0;
-  scanf("%s",a);
-  for(i=0;a[i]!='\0';i++)
+  char b[100];
+  int i,l=0,k=0;
+  scanf("%s",b);
+  for(i=0;b[i]!='\0';i++)
   {
       l++;
   }
-  if(a[0]=='s'||a[0]=='S')
+  if(b[0]=='s'||b[0]=='S'&&l==8||l==6&&b[l-1]=='y'||'Y')
   {
-      if(l==8||l==6)
-      printf("yes");
+   for(i=0;i<l;i++)
+   {
+     if(b[1]=='a'||'A'&&b[2]=='t'||'T'&&b[3]=='u'||'U'&&b[4]=='r'||'R'&&b[5]=='d'||'D'&&b[6]=='a'||'A'||b[1]=='u'||'U'&&b[2]=='n'||'N'&&b[3]=='d'||'D'&&b[4]=='a'||'A')
+       {
+         k++;
+       }
+   }
   }
-  else{
-      printf("no");
-  }
-  return 0;
+  if(k==l)
+  printf("yes");
+   else{
+     printf("no");
+   }
+  getch();
 }
