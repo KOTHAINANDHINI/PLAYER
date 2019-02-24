c8cdc150a12b60eb26ea36f3d4c0b8239ae2cb0a
KOTHAINANDHINI <46838428+KOTHAINANDHINI@users.noreply.github.com>
2019-02-24 20:43:52 +0530

    Create 52.C

---
+++ 52.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int a[100],N,K,i,j,p,c=a[0];
+    scanf("%d%d",&N,&N);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+   for(j=0;j<N+1-K;j++)
+   {
+       c=a[0];
+      for(i=0;i<N;i++)
+     {
+         if(a[i]>c)
+         {
+           c=a[i];
+           p=i;
+         }
+     }
+      a[p]=0;
+          
+   }
+   printf("%d",c);
+   return 0;
+}
