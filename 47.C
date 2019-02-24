b54365df115e56ecfe4ae64ab5206deb17d29c94
KOTHAINANDHINI <46838428+KOTHAINANDHINI@users.noreply.github.com>
2019-02-24 18:15:39 +0530

    Create 47.C

---
+++ 47.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if(a+b+c==180)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    getch();
+}
