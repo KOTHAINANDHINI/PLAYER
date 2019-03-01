962ff153ba9b50740a78dc9e5378964bbea5faee
KOTHAINANDHINI <46838428+KOTHAINANDHINI@users.noreply.github.com>
2019-03-01 20:44:50 +0530

    Rename 38.C to 39.C

---
+++ 39.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,p=1,l=0;
+    scanf("%d",&a);
+    for(i=0;i<=a/2;i++)
+    {
+        p=p*2;
+        if(a==p)
+        {
+            l=1;
+            break;
+        }
+    }
+    if(l==1)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+}
