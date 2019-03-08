422d4557cf04f862dda68f701be2ece1e25b8194
KOTHAINANDHINI <46838428+KOTHAINANDHINI@users.noreply.github.com>
2019-03-08 22:18:39 +0530

    Create 80.C

---
+++ 80.C
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int a[1000],i,n,j,k,c;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    c=a[0];
+    for(i=0;i<n;i++)
+    {
+       for(j=i+1;j<n;j++)
+       {
+           if(a[i]>a[j])
+           {
+               k=a[i]-a[j];
+           }
+           else
+           {
+               k=a[j]-a[i];
+           }
+           if(k<c)
+           {
+               c=k;
+           }
+       }
+    }
+    printf("%d",c);
+    return 0;
+}
