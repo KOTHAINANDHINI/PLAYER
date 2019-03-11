7192574aac09335370ae83337a01988f4f5ba2f6
KOTHAINANDHINI <46838428+KOTHAINANDHINI@users.noreply.github.com>
2019-03-11 21:01:54 +0530

    Create 90.C

---
+++ 90.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,k,h=1,f=1,r=1,i,j,m;
+    scanf("%d %d",&n,&k);
+    m=n-k;
+    for(i=1;i<=n;i++)
+    {
+        f=f*i;
+    }
+    for(i=1;i<=m;i++)
+    {
+        r=r*i;
+    }
+     for(i=1;i<=k;i++)
+    {
+        h=h*i;
+    }
+    int c;
+    c=f/r*h;
+    printf("%d\n",c);
+    return 0;
+}
