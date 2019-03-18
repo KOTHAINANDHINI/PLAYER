9ce28e51366413f1af1420ac593cc72b65ff8b88
KOTHAINANDHINI <46838428+KOTHAINANDHINI@users.noreply.github.com>
2019-03-18 15:33:31 +0530

    Create 91.C

---
+++ 91.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+   int r,b,n,bin=0;
+   scanf("%d",&n);
+   while(n)
+   {
+       r=n%2;
+       n=n/2;
+       bin=bin+b*r;
+       b=b*10;
+   }
+   printf("%d",bin);
+   return 0;
+}
