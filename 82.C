6e9b82f139c8a4b73fd0414042e3d4fe6e6adb93
KOTHAINANDHINI <46838428+KOTHAINANDHINI@users.noreply.github.com>
2019-03-25 03:16:19 +0530

    Create 82.C

---
+++ 82.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main() {
+int a[100],i,j,l=1,n;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+    scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+    for(j=i+1;j<n;j++){
+         l=a[i]&a[j];
+    }
+   
+}
+printf("%d",l);
+return 0;
+}
