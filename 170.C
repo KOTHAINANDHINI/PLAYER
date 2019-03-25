2c65399f576271b9a8165e39f1a86c2c21997e73
KOTHAINANDHINI <46838428+KOTHAINANDHINI@users.noreply.github.com>
2019-03-25 12:48:47 +0530

    Create 170.C

---
+++ 170.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    char a[1000],b;
+   scanf("%s",a);
+    int i,j,k=0,c=0;
+    for(i=0;a[i]!='\0';i++)
+    {
+        for(j=i+1;a[j]!='\0';j++)
+        {
+            if(a[i]==a[j])
+            {
+                k++;
+            }
+        }
+           
+         if(k>c)
+            {
+              c=k;
+             b=a[i];
+            }
+    }
+    printf("%d %c",k,b);
+    return 0;
+}
