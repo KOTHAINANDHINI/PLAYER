9265488cca3e3b70085c366dd2c8368a6584734a
KOTHAINANDHINI <46838428+KOTHAINANDHINI@users.noreply.github.com>
2019-03-25 12:10:42 +0530

    Update 167.C

--- 167.C
+++ 167.C
@@ -4,9 +4,10 @@ int main()
 {
     char a[1000];
     int i,j,f=0,l=0;
-    scanf("%s",a);
+    scanf("%[^\n]",a);
     for(i=0;a[i]!='\0';i++)
     {
+        if(a[i]!=' ')
         l++;
     }
     for(i=2;i<=l/2;i++)
