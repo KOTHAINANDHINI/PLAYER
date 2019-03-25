3921a4b45bf3eefcad6f9d9f6d014a566015abb6
KOTHAINANDHINI <46838428+KOTHAINANDHINI@users.noreply.github.com>
2019-03-25 05:31:11 +0530

    Create 136.C

---
+++ 136.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100],i,k,l=0;
+    scanf("%d %d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==k)
+        {
+            l++;
+        }
+    }
+    if(l>0)
+    {
+        printf("yes%d",l);
+    }
+    else
+    {
+        printf("no");
+    }
+}
